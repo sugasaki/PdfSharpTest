4a0657156e98b9cfc5e26e0a277024c0360b1a3e
sugasaki <sugasaki@gmail.com>
2018-07-18 13:48:52 +0900

    最初のコミット pdf作成、helloworldを追加

---
+++ PdfSharpTest/Form1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Windows.Forms;
+
+using System.Diagnostics;
+
+
+namespace PdfSharpTest
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            const string filename = "HelloWorld.pdf";
+
+            PdfCreator.HelloWorld(filename);
+
+
+            // ...and start a viewer.
+            Process.Start(filename);
+
+
+        }
+
+
+    }
+}
---
+++ PdfSharpTest/PdfCreator.cs
@@ -0,0 +1,48 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using PdfSharp;
+using PdfSharp.Drawing;
+using PdfSharp.Pdf;
+using PdfSharp.Pdf.IO;
+
+
+namespace PdfSharpTest
+{
+    public class PdfCreator
+    {
+
+        public static void HelloWorld(string filename)
+        {
+
+            // Create a new PDF document
+            PdfDocument document = new PdfDocument();
+            document.Info.Title = "Created with PDFsharp";
+
+            // Create an empty page
+            PdfPage page = document.AddPage();
+
+            // Get an XGraphics object for drawing
+            XGraphics gfx = XGraphics.FromPdfPage(page);
+
+            // Create a font
+            XFont font = new XFont("Verdana", 20, XFontStyle.BoldItalic);
+
+            // Draw the text
+            gfx.DrawString("Hello, World!", font, XBrushes.Black,
+              new XRect(0, 0, page.Width, page.Height),
+              XStringFormats.Center);
+
+            // Save the document...
+            document.Save(filename);
+
+
+        }
+
+
+    }
+}
